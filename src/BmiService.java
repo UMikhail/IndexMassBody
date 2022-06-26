2f6068edeeb5c24387053d60de17a28aba6c90ef
Михаил Уткин <mixail.rasto4u@yandex.ru>
2022-06-26 12:28:02 +0400

    Изменение параметров

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
-    public double calculate(double m, double h) {
-        double h2 = h * h;
-        double result = m / h2;
+    public double calculate(double weight, double height) {
+        double h2 = height * height;
+        double result = weight / h2;
         return result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double imt = service.calculate(80, 1.8);
-        System.out.println("Индекс массы тела: " + imt);
+        double bmi = service.calculate(80, 1.8);
+        System.out.println("Индекс массы тела: " + bmi);
     }
 }
